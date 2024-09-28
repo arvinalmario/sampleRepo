2f26fe90415ef0891f1d0b2f12df5413253c3eb7
Arvin <almarioarvhine.014@gmail.com>
2024-09-28 23:54:26 +0800

    added gitignore

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $array = ['A' => 'Ubas', 'B' => 'Santol', 'C' => 'Mangga', 'D' => 'Apol', 'E' => 'Bayavas'];
 $keys = array_keys( $array ); //built in function 
 function myFunction($prutas,$suse){
-    for($i = 0; $i < count($suse); $i++){
+    for($i = 0; $i < count($suse); $i++){ 
+        
         $value = $suse[$i];
         $key = $prutas[$value];
         echo 'Index: ' .$value .' Value: ' .$key .'<br>';
